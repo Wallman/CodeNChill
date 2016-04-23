66102409ee47079db2ef8fa4e7273cd8ac319d7a
Wallman <daniel.wallman94@gmail.com>
2016-04-23 15:38:43 +0200

    Initial commit

---
+++ Dijkstra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dijkstra
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
